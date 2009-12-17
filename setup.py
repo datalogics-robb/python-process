728549e1c5e22f75eb90d18554a858f8b7c97f2a
Kevin A. Mitchell <kam@datalogics.com>
2009-12-17 17:20:14 -0600

    Move Python modules into actual python_processes package directory.
    - Fixes problems with pip -e installs.
    
    - No more silly setup.py tricks.
    
    - Incremented version to 1.0.1.

--- setup.py
+++ setup.py
@@ -3,10 +3,9 @@
 from distutils.core import setup
 
 setup(name='python_processes',
-      version='1.0', # A DL-specific version number
+      version='1.0.1', # A DL-specific version number
       description='Extensions to the subprocess module',
       author='Benjamin Smedberg',
       author_email='benjamin@smedbergs.us',
       url='http://benjamin.smedbergs.us/blog/2006-11-09/adventures-in-python-launching-subprocesses/',
-      package_dir={'python_processes' : ''},
       packages=['python_processes'])
